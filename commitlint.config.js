20722acecab03f7f3744c5ba53fb2dd4f8830700
xuanlvzeng <xuanlvzeng@gmail.com>
2022-11-26 00:09:22 +0800

    feat: 项目初始化

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"]
+    ],
+    "subject-full-stop": [0, "never"],
+    "subject-case": [0, "never"]
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <h1>x-boot-v3</h1>
+    <span>scss1</span>
+    <a-button type="primary">Primary Button</a-button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  span {
+    font-size: 20px;
+  }
+}
+</style>
